de2bf046cc1052aa8b81c6ea23148c5e5933d651
hridoy <hasanhridot1367@gmail.com>
2023-07-18 17:40:07 -0700

    registration page done

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './My-Components/registration/registration.component';
+import { LoginComponent } from './My-Components/login/login.component';
+import { SocialLoginComponent } from './My-Components/social-login/social-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    LoginComponent,
+    SocialLoginComponent
   ],
   imports: [
     BrowserModule,
