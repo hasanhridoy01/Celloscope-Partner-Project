7152c4430759a435d87fc6725416326f3beb69c5
Fahim Foysal Emon <foysal@newroztech.com>
2023-07-21 02:27:52 +0600

    all schema design done

---
+++ backend/.history/models/AccoutManagerInfo_20230721022719.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+//Schema Design.....!
+const AccoutManagerInfoSchema = mongoose.Schema({
+    managerID: {
+    name: Number,
+    require: [true, "Please provide a Manager ID"]
+  },
+  managername: {
+    type: String,
+    required: [true, "Please provide a Manager Name"],
+    trim: true,
+    unique: [true, "Name must be unique"],
+    minLength: [5, "Name must be at least 3 character!"],
+    maxLength: [100, "Name is to large!"],
+  },
+  managerphone: {
+    type: number,
+    required: [true, "Please provide a Manager Phone!"]
+  },
+  manageremail: {
+    name: String,
+    require: [true, "Please provide Manager E-mail"],
+  },
+  systementrydate: {
+    name: String,
+    require: [true, "Please provide System Entry Date"]
+    birthDate: { type: Number, default: Date.now }
+  },
+  createAt: {
+    type: Date,
+    default: Date.now,
+  }
+},{timestamp: true});
+
+//Create Model.....!
+const AccoutManagerInfo = mongoose.model("AccoutManagerInfo", AccoutManagerInfoSchema);
+
+module.exports = AccoutManagerInfo;
