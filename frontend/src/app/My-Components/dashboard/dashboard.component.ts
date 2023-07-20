145974e6cb729dbe118086427fdfe5c62629b794
Fahim Foysal Emon <foysal@newroztech.com>
2023-07-21 03:58:53 +0600

    form data get done

--- frontend/src/app/My-Components/dashboard/dashboard.component.ts
+++ frontend/src/app/My-Components/dashboard/dashboard.component.ts
@@ -8,8 +8,24 @@ import { AuthService } from 'src/app/Shared/auth.service';
 })
 export class DashboardComponent {
   constructor(private auth: AuthService){ }
+
+  //Get All Data From BasicInfo Form
+  basicInfo(){
+    alert('hello this is basicInfo');
+  };
+
+  //Get All Data From partnerDetails Form
+  partnerDetails(){
+    alert('hello this is partnerDetail');
+  };
+
+  //Get All Data From accountManagerInfo Form
+  accountManagerInfo(){
+    alert('hello this is accountManagerInfo');
+  }
+
   //logout user....!
   logout(){
     this.auth.logout();
-  }
+  };
 }
