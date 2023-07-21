e935137de9990e4944ecbb0864a8d82cda880229
Zahid Hasan Hridoy <hasanhridoy1367@gmail.com>
2023-07-22 01:41:25 +0600

    all done

--- backend/.history/controllers/accountmanager.controller_20230722012538.js
+++ backend/.history/controllers/accountmanager.controller_20230722012538.js
@@ -26,23 +26,24 @@ exports.getAccoutManagerInfo = async(req, res, next) => {
 
 //Create a new AccoutManagerInfo form Database.............!
 exports.createAccoutManagerInfo = async(req, res, next) => {
-  try {
-    //Create or Save..!
-    const accoutManagerInfo = new AccoutManagerInfo(req.body);
-    //Save Data from Database..!
-    const result = await accoutManagerInfo.save();
-    //Response Send a message with user...! 
-    res.status(200).json({
-      status: "Successful!",
-      message: "Data Inserted SuccessFul!",
-      data: result
-    });
-  } catch (error) {
-    //If any Error Send a message with user...!
-    res.status(400).json({
-      status: "Failed!",
-      message: "Data Inserted UnSuccessful!",
-      error: error.message
-    });
-  }
+  console.log(req.body);
+  // try {
+  //   //Create or Save..!
+  //   const accoutManagerInfo = new AccoutManagerInfo(req.body);
+  //   //Save Data from Database..!
+  //   const result = await accoutManagerInfo.save();
+  //   //Response Send a message with user...! 
+  //   res.status(200).json({
+  //     status: "Successful!",
+  //     message: "Data Inserted SuccessFul!",
+  //     data: result
+  //   });
+  // } catch (error) {
+  //   //If any Error Send a message with user...!
+  //   res.status(400).json({
+  //     status: "Failed!",
+  //     message: "Data Inserted UnSuccessful!",
+  //     error: error.message
+  //   });
+  // }
 };
